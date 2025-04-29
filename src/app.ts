afa42bc3abd9c399f41b3c014fbf166488d54849
voyachekrv <voyachekrv@gmail.com>
2025-04-29 16:53:01 +0300

    Добавлен Swagger

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ import loggerMiddleware from './middlewares/logger-middleware';
 import requestRouter from './routers/request-router';
 import { register } from 'prom-client';
 import { errorHandler } from './middlewares/error-handler-middleware';
+import swaggerUi from 'swagger-ui-express';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
+import * as path from 'path';
 
 const env = dotenv.config();
 dotenvExpand.expand(env);
@@ -17,11 +21,15 @@ app.use(express.json());
 app.use(errorHandler);
 app.use(loggerMiddleware);
 
+const swaggerDocument = yaml.load(fs.readFileSync(path.join(process.cwd(), 'docs', 'openapi.yml'), 'utf8')) as object;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/metrics', async (req: Request, res: Response) => {
   res.set('Content-Type', register.contentType);
   res.end(await register.metrics());
 });
 
-app.use('/request', requestRouter);
+app.use('/api/request', requestRouter);
 
 export default app;
